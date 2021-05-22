dc32e6c0e7f65e230038617afd5bfd6921fd3367
Abyalew Ambaneh <77543474+AbyalewAmbaneh@users.noreply.github.com>
2021-05-22 17:21:54 -0500

    Add files via upload

---
+++ Complex mapping/exercise-06-1/src/main/java/edu/miu/cs/cs544/lab6_1/Payment.java
@@ -0,0 +1,27 @@
+package edu.miu.cs.cs544.lab6_1;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter
+@ToString
+@Embeddable
+public class Payment { 
+
+	private String paydate;
+	private double amount;
+	
+	public Payment(String paydate, double amount) {
+		super();
+		this.paydate = paydate;
+		this.amount = amount;
+	}
+	
+	
+} 
